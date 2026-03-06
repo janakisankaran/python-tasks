77328121224e5ff24c947e2899c2daa8a34689bd
janakisankaran <149142437+janakisankaran@users.noreply.github.com>
2026-03-06 21:28:00 +0530

    Add files via upload

---
+++ area_module.py
@@ -0,0 +1,10 @@
+import math
+
+def circle_area(r):
+    return math.pi * r * r
+
+def square_area(a):
+    return a * a
+
+def rectangle_area(l, b):
+    return l * b
---
+++ student_module.py
@@ -0,0 +1,4 @@
+def display_student():
+    student = {"Name": "Ravi", "Age": 20, "Course": "BSc"}
+    for key, value in student.items():
+        print(key, ":", value)
